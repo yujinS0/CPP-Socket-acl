50835ecef80f961394cfba50b2ce2eaa5636c433
U_Zin <930yujin@gmail.com>
2024-08-13 18:47:19 +0900

    ADD TCPEcoServerAcl in sln

---
+++ TCPEcoServerAcl/TCPEcoServerAcl.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "tcp.h"
+
+int main() {
+    try {
+        std::cout << "__cplusplus value: " << __cplusplus << std::endl; // C++ 버전 확인용
+        acl::acl_cpp_init();  // ACL 초기화
+        run_tcp_server();  // TCP 서버 실행 함수 호출
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Standard exception: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception caught" << std::endl;
+    }
+
+    return 0;
+}
