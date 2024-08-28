2f9ecca3c72d97cfb0e77bfc51934b9498a28b44
U_Zin <930yujin@gmail.com>
2024-08-28 12:03:22 +0900

    FIX main.cpp 반복 실행

--- SyncObjectsAcl/main.cpp
+++ SyncObjectsAcl/main.cpp
@@ -13,40 +13,45 @@ int main_ms();      // fiber_mutex_stat 예제 함수
 int main() {
     std::string input;
 
-    std::cout << "Select the synchronization object to test:\n";
-    std::cout << "1. fiber_cond\n";
-    std::cout << "2. fiber_event\n";
-    std::cout << "3. fiber_lock\n";
-    std::cout << "4. fiber_mutex\n";
-    std::cout << "5. fiber_sem\n";
-    std::cout << "6. fiber_rwlock\n";
-    std::cout << "7. fiber_mutex_stat (deadlock check)\n";
-    std::cout << "Enter your choice: ";
-    std::cin >> input;
+    while (true) {
+        std::cout << "Select the synchronization object to test:\n";
+        std::cout << "1. fiber_cond\n";
+        std::cout << "2. fiber_event\n";
+        std::cout << "3. fiber_lock\n";
+        std::cout << "4. fiber_mutex\n";
+        std::cout << "5. fiber_sem\n";
+        std::cout << "6. fiber_rwlock\n";
+        std::cout << "7. fiber_mutex_stat (deadlock check)\n";
+        std::cout << "Enter your choice (or 'q' to quit):  ";
+        std::cin >> input;
 
-    if (input == "1") {
-        return main_fcond();
-    }
-    else if (input == "2") {
-        return main_fevent();
-    }
-    else if (input == "3") {
-        return main_flock();
-    }
-    else if (input == "4") {
-        return main_fmutex();
-    }
-    else if (input == "5") {
-        return main_sem();
-    }
-    else if (input == "6") {
-        return main_rw();
-    }
-    else if (input == "7") {
-        return main_ms();
-    }
-    else {
-        std::cout << "Invalid choice!" << std::endl;
-        return 1;
+        if (input == "1") {
+            main_fcond();
+        }
+        else if (input == "2") {
+            main_fevent();
+        }
+        else if (input == "3") {
+            main_flock();
+        }
+        else if (input == "4") {
+            main_fmutex();
+        }
+        else if (input == "5") {
+            main_sem();
+        }
+        else if (input == "6") {
+            main_rw();
+        }
+        else if (input == "7") {
+            main_ms();
+        }
+        else if (input == "q") {
+            std::cout << "Exiting..." << std::endl;
+            break;
+        }
+        else {
+            std::cout << "Invalid choice! Please try again." << std::endl;
+        }
     }
 }
